43829a0d4fe5e9541541f169ca19af7e8aa78600
Abdul Rahaman <a.rahmanshah@gmail.com>
2015-03-15 08:31:27 +0300

    changed sql to get posts

--- index.js
+++ index.js
@@ -21,15 +21,21 @@ function getTweets(req, res) {
 
     console.log("Connected as ID: ", connection.threadId);
 
-    connection.query("SELECT * FROM tweets", function(err, rows, fields) {
+    connection.query("SELECT tweets.*, users.screen_name FROM tweets, users WHERE tweets.user_id = users.user_id ORDER BY tweets.created_at", function(err, rows, fields) {
       connection.release();
       if(!err) {
-        res.json(rows[1]['post_content']);
+        res.json(rows);
         return;
       }
     });
   });
 }
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+})
 
 app.get("/getTweets", function(req, res) {
   getTweets(req, res);
